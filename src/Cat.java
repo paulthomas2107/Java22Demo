53a27af58393df1ca512e46474a02d84e620215c
paulthomas2107 <paulthomas2107@gmail.com>
2024-05-01 09:49:14 +0100

    More

---
+++ src/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+
+        Cat() {
+            System.out.println("Build cat ....");
+        }
+        void meow() {
+            System.out.println("Meow x ....");
+        }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world! " + System.getProperty("java.version") );
 
-        // unnamed var
+        // unnamed var _ underscore
         Cat _ = new Cat();
+
+        try {
+            int myInit = Integer.parseInt("sss");
+        } catch (NumberFormatException _) { // ignores
+
+        }
+
+        List<Cat> cats = List.of(new Cat(), new Cat(), new Cat());
+        cats.forEach(_ -> System.out.println("Yay Cats"));
+
     }
+
+    // String
+    String name = "Paul";
+    int age = 21;
+    String message = STR."Hello \{name} age is \{age}";
+
+
+
 }
 
-class Cat {
-
-}
