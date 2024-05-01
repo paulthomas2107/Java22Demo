a8721d0507e6043176594b94601577ef699d636d
paulthomas2107 <paulthomas2107@gmail.com>
2024-05-01 09:22:39 +0100

    Initial

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world! " + System.getProperty("java.version") );
+
+        // unnamed var
+        Cat _ = new Cat();
+    }
+}
+
+class Cat {
+
+}
